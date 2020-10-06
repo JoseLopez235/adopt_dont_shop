cc1f517a4f658fa6a5d0274dd01ef59ae61df6a9
Jose Lopez <joselopez11394@gmail.com>
2020-10-05 20:44:34 -0400

    adds story 4

--- app/controllers/shelter_controller.rb
+++ app/controllers/shelter_controller.rb
@@ -1,7 +1,23 @@
 class ShelterController < ApplicationController
 
   def index
-    @shelters = ['shelter 1', 'shelter 2', 'shelter 3']
+    @shelters = Shelter.all
   end
 
+  def new
+  end
+
+  def create
+    shelter = Shelter.new({
+      name: params[:shelter][:name],
+      address: params[:shelter][:address],
+      city: params[:shelter][:city],
+      state: params[:shelter][:state],
+      zip: params[:shelter][:zip]
+      })
+
+    shelter.save
+
+    redirect_to '/shelters'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/shelters', to: 'shelter#index'
+
+  get '/shelters/new', to: 'shelter#new'
+
+  post '/shelters', to: 'shelter#create'
 end
